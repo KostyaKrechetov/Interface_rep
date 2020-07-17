4e368f8fb27fbe8f83e5003a97fde52b4625f798
Konstantin Krechetov <33196204+KostyaKrechetov@users.noreply.github.com>
2020-07-17 13:03:07 +0500

    Добавлена недостающая папка Design

---
+++ Design/Data_tab_stylesheet — копия.py
@@ -0,0 +1,28 @@
+// Control the tab-bar with respect to the QTabWidget
+QTabWidget::tab-bar {
+        left: 5px;
+}
+
+// Control the look of the tab in general
+QTabBar::tab {
+    border-top-left-radius: 4px;
+    border-top-right-radius: 4px;
+}
+
+// Control the look of the tab when it is selected
+QTabBar::tab:selected
+{
+        // Add css parameters
+}
+
+// Control the look of the tab when hovering over it
+QTabBar::tab:hover
+{
+        // Add css parameters
+}
+
+// Control the look of the tab when it is not selected
+QTabBar::tab:!selected
+{
+        // Add css parameters
+}
---
+++ Design/Error_label_style.py
@@ -0,0 +1,6 @@
+QLabel
+{
+    border: 1px solid black;
+    background-color: white;
+    color: #d7182e;
+}
